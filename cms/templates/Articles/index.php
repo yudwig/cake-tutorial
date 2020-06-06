c84a60bbf0dd2c5f832d4c905afc8bc9e7e80ef8
yudwig <yudwig.dev@gmail.com>
2020-06-06 13:14:16 +0900

    Add delete action.

--- cms/templates/Articles/index.php
+++ cms/templates/Articles/index.php
@@ -20,9 +20,13 @@
             </td>
             <td>
                 <?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?>
+                <?= $this->Form->postLink(
+                    '削除',
+                    ['action' => 'delete', $article->slug],
+                    ['confirm' => 'よろしいですか?'])
+                ?>
             </td>
         </tr>
     <?php endforeach; ?>
 
 </table>
-
