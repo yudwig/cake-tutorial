9dd4fd2f31d63d6ce1dd865b67164a196dde6807
yudwig <yudwig.dev@gmail.com>
2020-06-06 13:09:06 +0900

    Add edit view.

--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -2,13 +2,24 @@
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+use Cake\Event\EventInterface;
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config) : void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
 
--- cms/templates/Articles/index.php
+++ cms/templates/Articles/index.php
@@ -1,23 +1,28 @@
 
 <h1>記事一覧</h1>
-<?= $this->Html->link('記事の追加', ['action' => 'add']) ?>
+<p><?= $this->Html->link("記事の追加", ['action' => 'add']) ?></p>
 <table>
     <tr>
         <th>タイトル</th>
         <th>作成日時</th>
+        <th>操作</th>
     </tr>
 
-    <!-- ここで、$articles クエリーオブジェクトを繰り返して、記事の情報を出力します -->
+    <!-- ここで、$articles クエリーオブジェクトを繰り返して、記事情報を出力します -->
 
     <?php foreach ($articles as $article): ?>
-    <tr>
-        <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
-        </td>
-        <td>
-            <?= $article->created->format(DATE_RFC850) ?>
-        </td>
-    </tr>
+        <tr>
+            <td>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+            </td>
+            <td>
+                <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+            <td>
+                <?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?>
+            </td>
+        </tr>
     <?php endforeach; ?>
+
 </table>
 
