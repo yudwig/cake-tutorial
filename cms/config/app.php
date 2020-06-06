085ed77ab401bd0bf25d5dc42dc1d813c949d65f
yudwig <yudwig.dev@gmail.com>
2020-06-06 12:00:48 +0900

    Add database config.

--- cms/config/app.php
+++ cms/config/app.php
@@ -275,10 +275,15 @@ return [
             'persistent' => false,
             'timezone' => 'UTC',
 
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'cake_cms',
+
             /**
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
              */
-            //'encoding' => 'utf8mb4',
+            'encoding' => 'utf8mb4',
 
             /**
              * If your MySQL server is configured with `skip-character-set-client-handshake`
